9218afc3fa6c4f078a6095243f6fa75a1c0a8325
Pawel Szulc <paul.szulc@gmail.com>
2020-12-02 18:10:40 +0100

    Add hlint to the project

---
+++ hlint.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import ./haskell-pkgs.nix
+}:
+
+let
+  hsPkgs = pkgs.haskell-nix.stackProject {
+    compiler-nix-name = "ghc8101";
+    modules = [];
+    src = pkgs.fetchFromGitHub {
+      owner = "ndmitchell";
+      repo = "hlint";
+      # 3.2.1 release
+      rev = "57b664d77d27845629c093f6479b6104cf6455e2";
+      sha256 = "0y0c7vk9p6wj7vx4cfn6h0mg62jy03d0s54ydhmwv5c7112bcpqf";
+    };
+  };
+in
+  hsPkgs.hlint.components.exes.hlint
--- shell.nix
+++ shell.nix
@@ -4,11 +4,13 @@
 
 let
   hsPkgs = import ./. { inherit pkgs; };
+  hlint = import ./hlint.nix { inherit pkgs; };
 in
   hsPkgs.shellFor {
     tools = { cabal = "3.2.0.0"; };
     buildInputs =
       [ pkgs.haskellPackages.ghcid
         pkgs.haskellPackages.hp2pretty
+        hlint
       ];
   }
