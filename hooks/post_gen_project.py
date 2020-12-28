ea1d5c60a2a507063c00c4e46ebb81cc940fedef
Pawel Szulc <paul.szulc@gmail.com>
2020-12-28 18:44:07 +0100

    Add enable emacs integration
    It adds .dir-locals.el that turns of stack checker from flycheck

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,10 @@ def remove(filepath):
         shutil.rmtree(filepath)
 
 create_direnv = '{{cookiecutter.create_direnv}}' == 'y'
+enable_emacs_integration = '{{cookiecutter.enable_emacs_integration}}' == 'y'
 
 if not create_direnv:
     remove('.envrc')
+
+if not enable_emacs_integration:
+    remove('.dir-locals.el')
---
+++ {{cookiecutter.project_name}}/.dir-locals.el
@@ -0,0 +1,2 @@
+;; this is a cabal project, thus we don't want to have stack flyckech enabled
+(setq-default flycheck-disabled-checkers '(haskell-stack-ghc))
