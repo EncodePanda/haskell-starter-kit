b764ee04e7e7de2939688da2e99a65e742fc514c
Pawel Szulc <paul.szulc@gmail.com>
2021-01-02 11:58:31 +0100

    Properly configure dir-locals

--- {{cookiecutter.project_name_slug}}/.dir-locals.el
+++ {{cookiecutter.project_name_slug}}/.dir-locals.el
@@ -1,2 +1,2 @@
-;; this is a cabal project, thus we don't want to have stack flyckech enabled
-(setq-default flycheck-disabled-checkers '(haskell-stack-ghc))
+;; this is a cabal project, thus we don't want to have stack flycheck enabled
+((haskell-mode . ((flycheck-disabled-checkers . (haskell-stack-ghc)))))
